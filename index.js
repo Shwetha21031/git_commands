69f652e73f31e733edd23173012600ed33ac687d
Shwetha21031 <Shwetha21031@gmail.com>
2023-11-03 11:57:30 +0530

    a variable

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log("welcome");
 console.log("changes in branch 1");
 console.log("this is changes in main");
 console.log("added another branch (branch-2)");
+let a = 10;
+a++
+console.log(a)
